e5065ed0b5efea544107fd50e6f32209c2d87a5d
Praveen Kumar <prvnkr.pk@gmail.com>
2020-04-24 15:04:28 +0530

    input data to feature conversion script added

---
+++ bert/vectorization.py
@@ -0,0 +1,149 @@
+"""
+Python script to convert training data to features
+"""
+
+import os
+import csv
+import tensorflow as tf
+
+from utils import logger
+from config import labels
+
+
+class InputExample(object):
+    """A single training/test example for simple sequence classification."""
+
+    def __init__(self, guid, text_a, text_b=None, label=None):
+        """Constructs an InputExample.
+    Args:
+      guid: Unique id for the example.
+      text_a: string. The untokenized text of the first sequence. For single
+        sequence tasks, only this sequence must be specified.
+      text_b: (Optional) string. The untokenized text of the second sequence.
+        Only must be specified for sequence pair tasks.
+      label: (Optional) string. The label of the example. This should be
+        specified for train and dev examples, but not for test examples.
+    """
+        self.guid = guid
+        self.text_a = text_a
+        self.text_b = text_b
+        self.label = label
+
+
+class InputFeatures(object):
+    """A single set of features of data."""
+
+    def __init__(
+        self, input_ids, input_mask, segment_ids, label_id, is_real_example=True
+    ):
+        self.input_ids = input_ids
+        self.input_mask = input_mask
+        self.segment_ids = segment_ids
+        self.label_id = label_id
+        self.is_real_example = is_real_example
+
+
+class DataProcessor(object):
+    """Base class for data converters for sequence classification data sets."""
+
+    def get_train_examples(self, data_dir):
+        """Gets a collection of `InputExample`s for the train set."""
+        raise NotImplementedError()
+
+    def get_dev_examples(self, data_dir):
+        """Gets a collection of `InputExample`s for the dev set."""
+        raise NotImplementedError()
+
+    def get_test_examples(self, data_dir):
+        """Gets a collection of `InputExample`s for prediction."""
+        raise NotImplementedError()
+
+    def get_labels(self):
+        """Gets the list of labels for this data set."""
+        raise NotImplementedError()
+
+    @classmethod
+    def _read_csv(cls, input_file, delimiter=",", header=True, quotechar=None):
+        """Reads a tab separated value file."""
+        with tf.io.gfile.GFile(input_file, "r") as f:
+            reader = csv.reader(f, delimiter=delimiter, quotechar=quotechar)
+            lines = []
+            for line in reader:
+                lines.append(line)
+            if header:
+                lines = lines[1:]
+            return lines
+
+
+class TextDataProcessor(DataProcessor):
+    def get_train_examples(self, data_dir):
+        return self._create_examples(
+            self._read_csv(os.path.join(data_dir, "train.csv"))
+        )
+
+    def get_dev_examples(self, data_dir):
+        return self._create_examples(
+            self._read_csv(os.path.join(data_dir, "valid.csv"))
+        )
+
+    def get_test_examples(self, data_dir):
+        return self._create_examples(self._read_csv(os.path.join(data_dir, "test.csv")))
+
+    def get_labels(self):
+        return labels
+
+    def _create_examples(self, lines):
+        examples = []
+        for line in lines:
+            examples.append(
+                InputExample(guid=None, text_a=line[1], text_b=None, label=line[0])
+            )
+        return examples
+
+
+def convert_examples_to_features(examples, label_list, max_seq_length, tokenizer):
+    """Convert a set of `InputExample`s to a list of `InputFeatures`."""
+
+    features = []
+    label_map = {label: i for i, label in enumerate(label_list)}
+    for (ex_index, example) in enumerate(examples):
+        if ex_index % 10000 == 0:
+            logger.info("Writing example %d of %d" % (ex_index, len(examples)))
+
+        tokens_a = tokenizer.tokenize(example.text_a)
+        tokens = []
+        segment_ids = []
+        tokens.append("[CLS]")
+        segment_ids.append(0)
+        for token in tokens_a:
+            tokens.append(token)
+            segment_ids.append(0)
+        tokens.append("[SEP]")
+        segment_ids.append(0)
+        input_ids = tokenizer.convert_tokens_to_ids(tokens)
+        input_mask = [1] * len(input_ids)
+        # Zero-pad up to the sequence length.
+        while len(input_ids) < max_seq_length:
+            input_ids.append(0)
+            input_mask.append(0)
+            segment_ids.append(0)
+        assert len(input_ids) == max_seq_length
+        assert len(input_mask) == max_seq_length
+        assert len(segment_ids) == max_seq_length
+        label_id = label_map[example.label]
+        if ex_index < 5:
+            logger.info("*** Example ***")
+            logger.info("tokens: %s" % " ".join([str(x) for x in tokens]))
+            logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
+            logger.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
+            logger.info("segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
+            logger.info("label: %s (id = %d)" % (example.label, label_id))
+        feature = InputFeatures(
+            input_ids=input_ids,
+            input_mask=input_mask,
+            segment_ids=segment_ids,
+            label_id=label_id,
+            is_real_example=True,
+        )
+        features.append(feature)
+    return features
---
+++ config.py
@@ -0,0 +1,20 @@
+"""
+Python script containing configurations for classification task
+"""
+
+labels = [
+    "worry",
+    "neutral",
+    "sadness",
+    "happiness",
+    "fear",
+    "love",
+    "anger",
+    "surprise",
+    "hate",
+    "fun",
+    "relief",
+    "empty",
+    "enthusiasm",
+    "boredom",
+]
